61477ab5026650d45b62988a46f26dde83110223
hhv1996 <hhv1996@gmail.com>
2021-11-16 20:17:30 -0300

    se corrigio el import de bootstrap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Navbar from './components/NavBar/NavBar'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 ReactDOM.render(
   <React.StrictMode>
