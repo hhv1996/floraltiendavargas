b8f88a51b6bc288cca18559271c8f856d00a87dc
hhv1996 <hhv1996@gmail.com>
2021-11-16 20:05:36 -0300

    se arreglo className

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,24 +3,24 @@ import Logo from './logo.png'
 
 const NavBar = ()=>{
     return(
-		<nav class="navbar navbar-expand-lg navbar-light ">
-				<div class="container">
-					<a class="navbar-brand" href="#"><img src={Logo} alt="" width="110px" /></a>
-					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
+		<nav className="navbar navbar-expand-lg navbar-light ">
+				<div className="container">
+					<a className="navbar-brand" href="#"><img src={Logo} alt="" width="110px" /></a>
+					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
 					</button>
-					<div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-						<div class="navbar-nav">
-							<a class="nav-link active" href="#">INICIO</a>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+					<div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+						<div className="navbar-nav">
+							<a className="nav-link active" href="#">INICIO</a>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     PRODUCTOS
                                 </a>
                             </li>
-                            <a class="nav-link " href="#">NOSOTRAS</a>
-                            <a class="nav-link " href="#">FLOR DE DATA</a>
-                            <a class="nav-link " href="#">COMO COMPRAR</a>
-                            <a class="nav-link " href="#">CONTACTO</a>
+                            <a className="nav-link " href="#">NOSOTRAS</a>
+                            <a className="nav-link " href="#">FLOR DE DATA</a>
+                            <a className="nav-link " href="#">COMO COMPRAR</a>
+                            <a className="nav-link " href="#">CONTACTO</a>
 						</div>
 					</div>
 				</div>
