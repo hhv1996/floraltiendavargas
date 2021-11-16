0729eb37324ee20798d8c24eb876c019cf0cf6fd
hhv1996 <hhv1996@gmail.com>
2021-11-16 20:21:23 -0300

    se arreglaron los href

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,22 +5,22 @@ const NavBar = ()=>{
     return(
 		<nav className="navbar navbar-expand-lg navbar-light ">
 				<div className="container">
-					<a className="navbar-brand" href="#"><img src={Logo} alt="" width="110px" /></a>
+					<a className="navbar-brand" href="/#"><img src={Logo} alt="" width="110px" /></a>
 					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 					</button>
 					<div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
 						<div className="navbar-nav">
-							<a className="nav-link active" href="#">INICIO</a>
+							<a className="nav-link active" href="/#">INICIO</a>
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a className="nav-link dropdown-toggle" href="/#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     PRODUCTOS
                                 </a>
                             </li>
-                            <a className="nav-link " href="#">NOSOTRAS</a>
-                            <a className="nav-link " href="#">FLOR DE DATA</a>
-                            <a className="nav-link " href="#">COMO COMPRAR</a>
-                            <a className="nav-link " href="#">CONTACTO</a>
+                            <a className="nav-link " href="/#">NOSOTRAS</a>
+                            <a className="nav-link " href="/#">FLOR DE DATA</a>
+                            <a className="nav-link " href="/#">COMO COMPRAR</a>
+                            <a className="nav-link " href="/#">CONTACTO</a>
 						</div>
 					</div>
 				</div>
